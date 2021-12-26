a85ff1e1344daae56d9824d60a32b58d684d7ec6
notequal21 <kalorigen@gmail.com>
2021-12-27 00:49:58 +0300

    store was updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import style from "./app.module.scss"
 import projects from "./store/projects.json"
 
 function App() {
+
+  let projectList = projects.map(item => item.Id)
+
   return (
     <>
-
+      {projectList}
     </>
   );
 }
