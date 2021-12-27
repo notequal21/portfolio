0c6eb0cdf1f345f04996a22833882e1bd53cba4a
notequal21 <kalorigen@gmail.com>
2021-12-27 09:39:19 +0300

    added img folder

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import style from "./app.module.scss"
 import projects from "./store/projects.json"
 
 function App() {
-
-  let projectList = projects.map(item => item.Id)
-
   return (
     <>
-      {projectList}
+
     </>
   );
 }
