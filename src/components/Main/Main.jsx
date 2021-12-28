0924af419fcb1f0c6eec8950d2b5e7b63dbb3c1e
notequal21 <kalorigen@gmail.com>
2021-12-28 14:21:32 +0300

    added UI for main block in Main Component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,30 @@
+import style from './Main.module.scss'
+import myImg from '../../assets/img/components/main/img.jpg'
 
 let Main = () => {
   return (
-    <>
-      MAIN
-    </>
+    <div className={`${style.main}`}>
+      <div className="container">
+        <div className={`${style.mainBody}`}>
+          <div className={`${style.mainCol}`}>
+            <div className={`${style.mainCol__image}`}>
+              <img src={myImg} alt='img' />
+            </div>
+          </div>
+          <div className={`${style.mainCol}`}>
+            <div className={`${style.mainCol__name}`}>
+              Кирилл Махнёв
+            </div>
+            <div className={`${style.mainCol__pos}`}>
+              Frontend Developer
+            </div>
+            <div className={`${style.mainCol__about}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, corporis laudantium, tenetur nobis, quo sapiente aliquid cumque ea blanditiis architecto quod animi ipsam earum eius natus aut quia quae odit.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
