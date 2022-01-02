5ed831bce114b4eaab4a6c5292cefdb66732230a
notequal21 <kalorigen@gmail.com>
2022-01-02 21:06:40 +0300

    some UI updates in filter

--- src/components/popups/Filter/Filter.jsx
+++ src/components/popups/Filter/Filter.jsx
@@ -3,159 +3,70 @@ import './Filter.scss'
 import store from '../../../store/store'
 
 let FilterPopup = ({ applyFilterType }) => {
-  if (store.filterType === 'react') {
-    return (
-      <Popup
-        trigger={<button className="button"> FILTER </button>}
-        modal
-        nested
-        lockScroll
-      >
-        {close => (
-          <div className='filter'>
-            <button className='filter__close' onClick={close}>
-              &times;
-            </button>
-            <div className='filter__title'> Filter </div>
-            <div className='filterBody'>
-              <button onClick={() => {
-                close()
-                applyFilterType('react')
-              }} className='filterBody__item active'>
-                React
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('multipage')
-              }} className='filterBody__item'>
-                Multipages
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('landing')
-              }} className='filterBody__item'>
-                Landings
-              </button>
-            </div>
+  return (
+    <Popup
+      trigger={<button className="filterButton"> FILTER </button>}
+      modal
+      nested
+      lockScroll
+    >
+      {close => (
+        <div className='filter'>
+          <button className='filter__close' onClick={close}>
+            &times;
+          </button>
+          <div className='filter__title'> Filter </div>
+          <div className='filterBody'>
+            {
+              store.filterType === 'react'
+                ? <button onClick={() => {
+                  close()
+                  applyFilterType('react')
+                }} className='filterBody__item active'>
+                  React
+                </button>
+                : <button onClick={() => {
+                  close()
+                  applyFilterType('react')
+                }} className='filterBody__item'>
+                  React
+                </button>
+            }
+            {
+              store.filterType === 'multipage'
+                ? <button onClick={() => {
+                  close()
+                  applyFilterType('multipage')
+                }} className='filterBody__item active'>
+                  Multipages
+                </button>
+                : <button onClick={() => {
+                  close()
+                  applyFilterType('multipage')
+                }} className='filterBody__item'>
+                  Multipages
+                </button>
+            }
+            {
+              store.filterType === 'landing'
+                ? <button onClick={() => {
+                  close()
+                  applyFilterType('landing')
+                }} className='filterBody__item active'>
+                  Landings
+                </button>
+                : <button onClick={() => {
+                  close()
+                  applyFilterType('landing')
+                }} className='filterBody__item'>
+                  Landings
+                </button>
+            }
           </div>
-        )}
-      </Popup>
-    )
-  } else if (store.filterType === 'multipage') {
-    return (
-      <Popup
-        trigger={<button className="button"> FILTER </button>}
-        modal
-        nested
-        lockScroll
-      >
-        {close => (
-          <div className='filter'>
-            <button className='filter__close' onClick={close}>
-              &times;
-            </button>
-            <div className='filter__title'> Filter </div>
-            <div className='filterBody'>
-              <button onClick={() => {
-                close()
-                applyFilterType('react')
-              }} className='filterBody__item'>
-                React
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('multipage')
-              }} className='filterBody__item active'>
-                Multipages
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('landing')
-              }} className='filterBody__item'>
-                Landings
-              </button>
-            </div>
-          </div>
-        )}
-      </Popup>
-    )
-  } else if (store.filterType === 'landing') {
-    return (
-      <Popup
-        trigger={<button className="button"> FILTER </button>}
-        modal
-        nested
-        lockScroll
-      >
-        {close => (
-          <div className='filter'>
-            <button className='filter__close' onClick={close}>
-              &times;
-            </button>
-            <div className='filter__title'> Filter </div>
-            <div className='filterBody'>
-              <button onClick={() => {
-                close()
-                applyFilterType('react')
-              }} className='filterBody__item'>
-                React
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('multipage')
-              }} className='filterBody__item'>
-                Multipages
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('landing')
-              }} className='filterBody__item active'>
-                Landings
-              </button>
-            </div>
-          </div>
-        )}
-      </Popup>
-    )
-  } else {
-    return (
-      <Popup
-        trigger={<button className="button"> FILTER </button>}
-        modal
-        nested
-        lockScroll
-      >
-        {close => (
-          <div className='filter'>
-            <button className='filter__close' onClick={close}>
-              &times;
-            </button>
-            <div className='filter__title'> Filter </div>
-            <div className='filterBody'>
-              <button onClick={() => {
-                close()
-                applyFilterType('react')
-              }} className='filterBody__item'>
-                React
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('multipage')
-              }} className='filterBody__item'>
-                Multipages
-              </button>
-              <button onClick={() => {
-                close()
-                applyFilterType('landing')
-              }} className='filterBody__item'>
-                Landings
-              </button>
-            </div>
-          </div>
-        )}
-      </Popup>
-    )
-  }
+        </div>
+      )}
+    </Popup>
+  )
 }
 
 export default FilterPopup
