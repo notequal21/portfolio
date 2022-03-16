bc905da22180596995f14195633df2bcf712ed0d
kirill.mahnev <kirill.mahnev@nutnet.ru>
2022-03-16 20:09:46 +0300

    refactored index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import { BrowserRouter } from "react-router-dom";
+const App = lazy(() => import("./App"));
+
+function AppLoader() {
+  return (
+    <Suspense fallback={<div className="loading">Loading...</div>}>
+      <App />
+    </Suspense>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppLoader />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
