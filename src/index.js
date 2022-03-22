282fb6ff21edfd0d54c91bcda17a043f9d854304
notequal21 <kalorigen@gmail.com>
2022-03-22 18:26:10 +0300

    added store provider and installed redux-toolkit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import reportWebVitals from './reportWebVitals';
+import { store } from './store/store'
+import { Provider } from 'react-redux'
 
 import { HashRouter } from "react-router-dom";
 const App = lazy(() => import("./App"));
 
 function AppLoader() {
   return (
-    <Suspense fallback={<div className="loading">Loading...</div>}>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback={<div className="loading">Loading...</div>}>
+        <App />
+      </Suspense>
+    </Provider>
   );
 }
 
